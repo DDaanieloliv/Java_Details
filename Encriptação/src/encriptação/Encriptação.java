6089ba451f01a57da3a58f114e135bfb8ed9f669
DDaanieloliv <126197002+DDaanieloliv@users.noreply.github.com>
2023-08-30 15:41:15 -0300

    Update Encriptação.java

--- Encriptação/src/encriptação/Encriptação.java
+++ Encriptação/src/encriptação/Encriptação.java
@@ -4,8 +4,8 @@ import java.util.Scanner;                                                       
 
 public class Encriptação {
     public static void main(String[] args) {
-       try (//Scanner entrada = new Scanner(System.in);                            // CRIANDO VARIAVEL TIPO 'Scanner'. Cujo comando nesta linha 
-	Scanner entrad = new Scanner (System.in)) {									  // dá o nome de 'entrada' 'System.in' REPRESENTA O TECLADO   
+       try (//Scanner entrada = new Scanner(System.in);                         // CRIANDO VARIAVEL TIPO 'Scanner'. Cujo comando nesta linha 
+	Scanner entrad = new Scanner (System.in)) {				// dá o nome de 'entrada' 'System.in' REPRESENTA O TECLADO   
            
        
        
